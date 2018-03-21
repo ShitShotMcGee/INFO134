b8ac90dbb5e61a93a42452fcbbc4b1c83a343254
wubwub <thomas.r.johansen@hotmail.com>
2018-03-21 12:27:04 +0100

    mer js

--- js/avansertSok1.js
+++ js/avansertSok1.js
@@ -1,31 +1,32 @@
+(window.onload = function () {
+    document.getElementById('søk').addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        /*
+        function Sok(herre, dame, rullestol, aapen, stellerom, gratis, klokkeslett, makspris){
+            this.herre = herre;
+            this.dame = dame;
+            this.rullestol = rullestol;
+            this.aapen = aapen;
+            this.stellerom = stellerom;
+            this.gratis = gratis;
+            this.klokkeslett = klokkeslett;
+            this.makspris = makspris;
+        }
+        */
 
-/*
-function Sok(herre, dame, rullestol, aapen, stellerom, gratis, klokkeslett, makspris){
-    this.herre = herre;
-    this.dame = dame;
-    this.rullestol = rullestol;
-    this.aapen = aapen;
-    this.stellerom = stellerom;
-    this.gratis = gratis;
-    this.klokkeslett = klokkeslett;
-    this.makspris = makspris;
+
+        var skjekk = document.getElementById('herre');
+
+        skjekk.addEventListener('click', function skjekk2(toalett.toaletter) {
+            if (skjekk.checked) {
+                console.log('woop');
+            } else {
+                console.log('buu');
+            }
+        })
+
+    }
 }
 
-    var herre = document.getElementById.innerHTML(herre).checked;
-    var dame = document.getElementById.innerHTML(dame).checked;
-    var rullestol = document.getElementById.innerHTML(rullestol).checked;
-    var aapen = document.getElementById.innerHTML(aapen).checked;
-    var stellerom = document.getElementById.innerHTML(stellerom).checked;
-    var gratis = document.getElementById.innerHTML(gratis).checked;
-    var klokkeslett = document.getElementById.innerHTML(klokkeslett).value;
-    var makspris = document.getElementById.innerHTML(makspris).value;
 
-
-    const checkHerre = document.getElementById('herre');
-    checkHerre.addEventListener('change', function (e) {
-        if(checkHerre.checked){
-
-        }
-    })
-*/
 
