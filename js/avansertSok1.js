b5dfee601abce528e4f9e08075cdd53e38341604
wubwub <thomas.r.johansen@hotmail.com>
2018-03-21 13:09:17 +0100

    PROGRESS

--- js/avansertSok1.js
+++ js/avansertSok1.js
@@ -1,32 +1,40 @@
-(window.onload = function () {
-    document.getElementById('søk').addEventListener('submit', function (evt) {
-        evt.preventDefault();
-        /*
-        function Sok(herre, dame, rullestol, aapen, stellerom, gratis, klokkeslett, makspris){
-            this.herre = herre;
-            this.dame = dame;
-            this.rullestol = rullestol;
-            this.aapen = aapen;
-            this.stellerom = stellerom;
-            this.gratis = gratis;
-            this.klokkeslett = klokkeslett;
-            this.makspris = makspris;
-        }
-        */
-
-
-        var skjekk = document.getElementById('herre');
-
-        skjekk.addEventListener('click', function skjekk2(toalett.toaletter) {
-            if (skjekk.checked) {
-                console.log('woop');
-            } else {
-                console.log('buu');
-            }
-        })
-
-    }
+function Sok(herre, dame, rullestol, aapen, stellerom, gratis, klokkeslett, makspris) {
+    this.herre = herre;
+    this.dame = dame;
+    this.rullestol = rullestol;
+    this.aapen = aapen;
+    this.stellerom = stellerom;
+    this.gratis = gratis;
+    this.klokkeslett = klokkeslett;
+    this.makspris = makspris;
 }
+
+
+var searchObj = {
+    herre: "",
+    dame: "",
+    rulllestol: "",
+    stellerom: "",
+    aapen: "",
+    gratis: ""
+};
+var skjekkHerre = document.getElementById('herre');
+var skjekkDame = document.getElementById('dame');
+var skjekkRullestol = document.getElementById('rullestol');
+var skjekkStellerom = document.getElementById('stellerom');
+var skjekkAapen = document.getElementById('aapen');
+var skjekkGratis = document.getElementById('gratis');
+
+skjekkHerre.addEventListener('click', function () {
+    if (skjekkHerre.checked) {
+        
+    } else {
+        return false;
+    }
+})
+
+
+
 
 
 
