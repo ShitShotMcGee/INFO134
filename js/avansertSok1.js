2d52fab5f2fc51050c4e155e6eaf5bfc3195b8a0
wubwub <thomas.r.johansen@hotmail.com>
2018-03-21 15:01:55 +0100

    PROGRESS V.2

--- js/avansertSok1.js
+++ js/avansertSok1.js
@@ -11,27 +11,78 @@ function Sok(herre, dame, rullestol, aapen, stellerom, gratis, klokkeslett, maks
 
 
 var searchObj = {
-    herre: "",
-    dame: "",
-    rulllestol: "",
-    stellerom: "",
+    herre: [],
+    dame: [],
+    rulllestol: [],
+    stellerom: [],
     aapen: "",
-    gratis: ""
+    gratis: []
 };
-var skjekkHerre = document.getElementById('herre');
-var skjekkDame = document.getElementById('dame');
-var skjekkRullestol = document.getElementById('rullestol');
-var skjekkStellerom = document.getElementById('stellerom');
-var skjekkAapen = document.getElementById('aapen');
-var skjekkGratis = document.getElementById('gratis');
 
-skjekkHerre.addEventListener('click', function () {
-    if (skjekkHerre.checked) {
-        
-    } else {
-        return false;
+(window.onload = function () {
+    var skjekkHerre = document.getElementById('herre');
+    var skjekkDame = document.getElementById('dame');
+    var skjekkRullestol = document.getElementById('rullestol');
+    var skjekkStellerom = document.getElementById('stellerom');
+    var skjekkAapen = document.getElementById('aapen');
+    var skjekkGratis = document.getElementById('gratis');
+
+    skjekkHerre.addEventListener('click', function () {
+        if (skjekkHerre.checked) {
+            searchObj.herre = 1;
+        }
+    });
+    skjekkDame.addEventListener('click', function () {
+        if (skjekkDame.checked) {
+            searchObj.dame = 1;
+        }
+    });
+    skjekkRullestol.addEventListener('click', function () {
+        if (skjekkRullestol.checked) {
+            searchObj.rullestol = 1;
+        }
+    });
+    skjekkStellerom.addEventListener('click', function () {
+        if (skjekkStellerom.checked) {
+            searchObj.stellerom = 1;
+        }
+    });
+    skjekkAapen.addEventListener('click', function () {
+        if (skjekkAapen.checked) {
+            searchObj.Aapen = 1;
+        }
+    });
+    skjekkGratis.addEventListener('click', function () {
+        if (skjekkGratis.checked) {
+            searchObj.Gratis = 0;
+        }
+    });
+
+
+function search() {
+    var searchResults = [];
+    var searchParam = Object.keys(searchObj);
+    for (i = 0; i < toaletter.length; i++) {
+        var truthChecker = [];
+        for (x = 0; x < searchParam; x++) {
+            if (toaletter[i][searchParam[x]] == searchObj[searchParam[x]]) {
+                truthChecker.push(true);
+            }
+            if (truthChecker.length == searchParam.length) {
+                searchResults.push(toaletter[i]);
+            }
+        }
     }
+    var src = document.getElementById('søk');
+    src.addEventListener('click', function () {
+        console.log(searchResults);
+    });
+}
 })
+
+
+
+
 
 
 
