5a58ef7cd7536ecc6c38a7cac622decf0c33ddc9
wubwub <thomas.r.johansen@hotmail.com>
2018-04-24 09:44:16 +0200

    HURRA

--- js/avansertSok1.js
+++ js/avansertSok1.js
@@ -1,25 +1,51 @@
-var link = 'https://hotell.difi.no/api/json/bergen/dokart?'
+var link = 'https://hotell.difi.no/api/json/bergen/dokart?';
 
-function hentData(url) {
+function getJson(url, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+// Typical action to be performed when the document is ready:
+            callback(xhttp.responseText)
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function setup(data) {
+    console.log(data);
+}
+
+function start() {
+    getJson(link, loadListe());
+}
+
+
+
+function hentData(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onload = function () {
         if (this.status == 200) {
-            var jsonData = JSON.parse(xhr.responseText);
+            callback(xhr.responseText);
         }
-     xhr.send();
+        xhr.onerror = function () {
+            alert('Feil ved innhenting...');
+        }
+        xhr.send();
+    }
 }
 
 
 function loadListe() {
-            hentData(link);
-            var text;
-            text = "<ol>";
-            for (i = 0; i < jsonData.entries.length; i++) {
-                text += "<li>" + jsonData.entries[i].plassering + "</li>";
-            }
-            text += "</ol>";
-            document.getElementById('liste').innerHTML = text;
+    hentData(link);
+    var text;
+    text = "<ol>";
+    for (i = 0; i < jsonData.entries.length; i++) {
+        text += "<li>" + jsonData.entries[i].plassering + "</li>";
+    }
+    text += "</ol>";
+    document.getElementById('liste').innerHTML = text;
 }
 
 //avansert søk
@@ -152,6 +178,9 @@ function initMap() {
     }
     xhr.send();
 }
+
+
+
 
 
 
