cf0c8b324a67f6a381c7038b73c4505a52ddfa83
Sindri Bjørnsson <>
2018-04-25 12:29:21 +0200

    fix fix  på lekeplasser

--- js/lekeplasser.js
+++ js/lekeplasser.js
@@ -1,18 +1,33 @@
-function lekeplassListe() {
-    xhr = new XMLHttpRequest();
-    url = 'https://hotell.difi.no/api/json/bergen/lekeplasser?';
-    xhr.open('GET', url, true);
+var lekeplass = 'https://hotell.difi.no/api/json/bergen/lekeplasser?';
+
+
+// en funksjon som henter data ut fra nettet
+function hentData(url, callback) {
+    var xhr = new XMLHttpRequest();
     xhr.onload = function () {
-        if(status == 200) {
-            var jsonData = JSON.parse(xhr.responseText);
-            var text;
-            text = "<ol>";
-            for (i = 0; i < jsonData.entries.length; i++) {
-                text += "<li>" + jsonData.entries[i].plassering + "</li>";
-            }
-            text += "</ol>";
-            document.getElementById('lekeplassliste').innerHTML = text;
+        if (this.status == 200) {
+            var loadData = JSON.parse(this.responseText);
+            callback(loadData);
         }
-    }
+    };
+    xhr.open('GET', url, true);
     xhr.send();
+
+}
+
+
+function lekeplassListe(data) {
+    var text;
+    text = "<ol>";
+    for (i = 0; i < data.entries.length; i++) {
+        text += "<li>" + data.entries[i].navn + "</li>";
+    }
+    text += "</ol>";
+    document.getElementById('listeLekeplass').innerHTML = text;
+}
+// funksjonen som gjør at list lastes in på siden, som tar in en url og en callback funksjon som parameter
+
+
+function startLekeplass() {
+    hentData(lekeplass, lekeplassListe);
 }
