037ab4af0a20ce1a75da2059fb57f5d403329df8
Sindri Bjørnsson <>
2018-04-26 15:10:30 +0200

    fix fix  på lekeplasser kart

--- js/lekeplasser.js
+++ js/lekeplasser.js
@@ -24,10 +24,100 @@ function lekeplassListe(data) {
     }
     text += "</ol>";
     document.getElementById('listeLekeplass').innerHTML = text;
+
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 60.391011, lng: 5.325950},
+        zoom: 14
+    });
+    for (var i = 0; i < data.entries.length; i++) {
+        var dataKart = data.entries[i],
+            latLng = new google.maps.LatLng(dataKart.latitude, dataKart.longitude);
+
+        //Creating a marker and putting it on the map
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+
+        });
+    }
+
+
+
+
+
+
+
 }
 // funksjonen som gjør at list lastes in på siden, som tar in en url og en callback funksjon som parameter
 
 
 function startLekeplass() {
     hentData(lekeplass, lekeplassListe);
+}
+
+
+function finnleke() {
+
+    hentData(lekeplass, finn);
+}
+
+function kart() {
+    hentData(lekeplass,kart );
+
+}
+
+
+function finn(data) {
+    var sjekk = document.getElementById('finne');
+    var searchObj = {"navn": sjekk.value };
+    var searchResults = [];
+    var searchParam = Object.keys(searchObj);
+    for (i = 0; i < data.entries.length; i++) {
+        var truthChecker = 0;
+        for (x = 0; x < searchParam.length; x++) {
+            if (data.entries[i][searchParam[x]] === searchObj[searchParam[x]]) {
+                truthChecker++;
+            }
+
+        }
+        if (truthChecker === searchParam.length) {
+            searchResults.push(data.entries[i]);
+        }
+
+
+    }
+
+    if (searchResults.length > 0) {
+        var text;
+        text = "<ol>";
+        for (i = 0; i < searchResults.length; i++) {
+            text += "<li>" + searchResults[i].navn + "</li>";
+        }
+        text += "</ol>";
+        document.getElementById('listeLekeplass').innerHTML = text;
+    } else {
+
+        document.getElementById('listeLekeplass').innerHTML = "Det finnest ingen lekeplass med dette navnet";
+    }
+
+
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 60.391011, lng: 5.325950},
+        zoom: 11
+    });
+
+    for (var i = 0; i < searchResults.length; i++) {
+        var dataMap = searchResults[i],
+            latLng = new google.maps.LatLng(dataMap.latitude, dataMap.longitude);
+
+        // Creating a marker and putting it on the map
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+
+        });
+    }
+
 }
